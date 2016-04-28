1f0e1eff6dbd2abc7e0136de2dcf1822b6652dcb
Daniel <danleavitt0@gmail.com>
2016-04-28 13:00:28 -0700

    update html

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var renderer = new marked.Renderer()
 renderer.listitem = function (text) {
   if (/^\s*\[[x ]\]\s*/.test(text)) {
     text = text
-    .replace(/^\s*\[ \]\s*/, '<i class="empty checkbox icon"></i> ')
-    .replace(/^\s*\[x\]\s*/, '<i class="checked checkbox icon"></i> ')
+    .replace(/^\s*\[ \]\s*/, '<input type="checkbox" class="task-list-item-checkbox"> ')
+    .replace(/^\s*\[x\]\s*/, '<input type="checkbox" class="task-list-item-checkbox" checked> ')
     return '<li style="list-style: none">' + text + '</li>'
   } else {
     return '<li>' + text + '</li>'
