250233010983e3cbb59d1c48a6ba1a2f4186d698
Daniel <danleavitt0@gmail.com>
2016-10-04 18:39:14 -0700

    add support for student actions

--- index.js
+++ index.js
@@ -23,17 +23,35 @@ marked.setOptions({
   smartypants: false
 })
 
+function wrapStudentActions (str) {
+  var re = new RegExp('(?:```student)([\\s\\S]*?)(```)', 'g')
+  var match = []
+  var newStr = str
+  while (match != null) {
+    match = re.exec(str)
+    if (match != null) {
+      newStr = newStr.replace(/(?:```student)([\s\S]*?)(```)/, '<div class="student-actions">' + marked(match[1]) + '</div>')
+    }
+  }
+  return newStr
+}
+
 function getGroups (str) {
-  var obj = {}
+  var arr = []
   var re = new RegExp('(\\d+)\\)\\s+([\\s\\S]*?)(?=(?:[^\\w]\\d+\\)|$))', 'g')
   var match = []
   while (match != null) {
     match = re.exec(str)
     if (match != null) {
-      obj[match[1]] = match[2]
+      arr.push('<div class="instruction"><div class="number">' +
+        match[1] +
+        '</div><div class="content">' +
+        marked(match[2]) +
+        '</div></div>'
+      )
     }
   }
-  return obj
+  return arr
 }
 
 function getRows (firstC, secondC) {
@@ -76,6 +94,18 @@ module.exports = {
         return marked(block.body)
       }
     },
+    studentActions: {
+      process: function (block) {
+        return '<div class="student-actions">' + marked(block.body) + '</div>'
+      }
+    },
+    activityBody: {
+      process: function (block) {
+        var body = wrapStudentActions(block.body)
+        var numberGroups = getGroups(body.replace(/\\/g, ''))
+        return '<div class="activity-body">' + numberGroups.join('') + '</div>'
+      }
+    },
     link: {
       process: function (block) {
         var text = block.kwargs.text
@@ -116,55 +146,40 @@ module.exports = {
         '</tbody></table>'
       }
     },
-    header: {
-      process: function (block) {
-        var title = block.kwargs.title
-        var icon = block.kwargs.icon
-        return '<div class="section-header"><div class="icon ' +
-        icon +
-        '"></div><div class="title">' +
-        title +
-        '</div></div></div>'
-      }
-    },
-    overviewSection: {
-      process: function (block) {
-        var title = block.kwargs.title
-        var icon = block.kwargs.icon
-        var hideHR = typeof (block.kwargs.hideHR) === 'undefined' ? false : block.kwargs.hideHR
-        var color = palette[Math.floor(Math.random() * palette.length)]
-        var display = hideHR ? 'none' : 'block'
-        return '<div class="overview-section"><div class="icon ' +
-        icon +
-        '"></div><div class="title">' +
-        title +
-        '</div></div><hr style="margin-top: 1em; border: 1px solid ' +
-        color +
-        '; height:1px; display: ' +
-        display +
-        '"></hr>'
-      }
-    },
-    length: {
-      process: function (block) {
-        var length = block.args[0]
-        return '<div class="length-wrapper"><div class="icon agenda"></div><div class="text"><strong>Length: </strong>' +
-        length +
-        '</div></div>'
-      }
-    },
-    activity: {
-      process: function (block) {
-        var length = block.kwargs.length
-        var activityName = block.kwargs.activityName
-        return '<div class="activity-header"><div class="activity-icon"></div><div class="activity"><div class="activity-number">' +
-          activityName +
-          '</div><div></div>' +
-          marked(block.body) +
-          '</div><div class="activity-length"><div class="length-icon"></div><span class="activity-length-header">length: </div>' +
-          length +
-          ' minutes</div></div>'
-      }
-    }
+    header: activityHeader,
+    activityHeader: activityHeader,
+    overviewSection: overviewHeader,
+    overviewHeader: overviewHeader
+  }
+}
+
+var activityHeader = {
+  process: function (block) {
+    var title = block.kwargs.title
+    var icon = block.kwargs.icon
+    return '<div class="section-header"><div class="icon ' +
+    icon +
+    '"></div><div class="title">' +
+    title +
+    '</div></div></div>'
+  }
+}
+
+var overviewHeader = {
+  process: function (block) {
+    var title = block.kwargs.title
+    var icon = block.kwargs.icon
+    var hideHR = typeof (block.kwargs.hideHR) === 'undefined' ? false : block.kwargs.hideHR
+    var color = palette[Math.floor(Math.random() * palette.length)]
+    var display = hideHR ? 'none' : 'block'
+    return '<div class="overview-section"><div class="icon ' +
+    icon +
+    '"></div><div class="title">' +
+    title +
+    '</div></div><hr style="margin-top: 1em; border: 1px solid ' +
+    color +
+    '; height:1px; display: ' +
+    display +
+    '"></hr>'
   }
 }
